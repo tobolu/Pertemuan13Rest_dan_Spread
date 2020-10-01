328aac2a283a9192ce5ae379f959deb9e65c41b0
tobolu <chmp287@gmail.com>
2020-10-01 06:26:56 -0700

    pertemuan 13

---
+++ index.js
@@ -0,0 +1,34 @@
+// Rest dan Spread Operator
+
+// Spread Operator
+// contoh variable array
+// Memecah array
+// duplicat array
+//const angka = [1, 2, 3, 4, 5];
+// const angka2 = [...angka]
+
+// angka[0] = 10;
+// console.log(angka);
+// console.log(...angka2);
+// // console.log(angka);
+// // console.log(...angka);
+
+//gabungkan array
+// const angka = [1, 2, 3, 4, 5];
+// const angka2 = [6, 7, 8, 9, 10];
+// const angka3 = [12, 13];
+
+// const gabung5 = [...angka, ...angka2, ...angka3];
+// console.log(gabung5);
+
+// const gabung6 = [...angka, ...angka2, ...angka3];
+// console.log(gabung6);
+
+//Menambah Properti Objek 
+const orang = {
+      nama: "Jhon",
+      umur: 33,
+}
+
+orang = {orang,pekerjaan "Gurun", pendidikan: "S2"};
+console.log(orang);
